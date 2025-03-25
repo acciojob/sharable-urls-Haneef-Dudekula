a58c49ae47ad630bdfc97dafa16ad7200b734c72
Haneef-Dudekula <dudekulahaneef2301@gmail.com>
2025-03-25 23:25:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// your code here
+function updateString() {
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+	let h3=document.getElementById("url");
+	if (name && year)
+	{
+		// console.log("h") 
+		h3.innerHTML= h3.innerHTML+`?name=${name}&year=${year}`;
+		// console.log(h3)
+	}
+	else if(name)
+	{
+		h3.innerHTML=h3.innerHTML+`?name=${name}`;
+	}
+	// console.log(name)
+	else if(year)
+	{
+		h3.innerHTML=h3.innerHTML+`?year=${year}`;
+	}
+
+}
